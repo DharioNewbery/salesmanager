ecddc08a2d1897a0f0d05e3fc4c71aff8d5b66f0
DharioNewbery <dharionew@gmail.com>
2026-01-27 12:27:36 -0300

    Added Vector.h and implemented Vector functions

---
+++ Vector.h
@@ -0,0 +1,140 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <stdexcept>
+
+template <typename T>
+class Vector {
+    private:
+    T* arr;
+    int size, capacity;
+
+    void redefineCapacity();
+    void checkBounds(int index);
+
+    public:
+    Vector();
+    ~Vector();
+
+    T pop();
+    T get(int index);
+
+    int find(T element);
+    int getSize();
+
+    void push(T element);
+    void remove(int index);
+    void insert(int index, T element);
+    void replace(int index, T element);
+};
+
+const int MIN_VECTOR_CAPACITY = 40;
+const int SIZE_INCREMENT = 5;
+// private
+template <typename T>
+Vector<T>::Vector(): size(0), capacity(MIN_VECTOR_CAPACITY)
+{
+    arr = new T[capacity];
+}
+
+template <typename T>
+Vector<T>::~Vector()
+{
+    delete[] arr;
+}
+
+template <typename T>
+void Vector<T>::redefineCapacity()
+{
+    if (capacity != size) return;
+
+    int newCapacity = capacity + SIZE_INCREMENT;
+    T* newArr = new T[newCapacity];
+
+    for (int i = 0; i < size; i++)
+        newArr[i] = arr[i];
+    
+    delete[] arr;
+    arr = newArr;
+}
+
+template <typename T>
+void Vector<T>::checkBounds(int index)
+{
+    if (index < 0 || index >= size)
+        throw std::out_of_range("index is out of bounds");
+}
+
+// public
+template <typename T>
+T Vector<T>::pop()
+{
+    if (size == 0)
+        throw std::out_of_range("tried to pop an empty vector");
+    size--;
+    return arr[size];
+}
+
+template <typename T>
+T Vector<T>::get(int index)
+{
+    checkBounds(index);
+    return arr[index];
+}
+
+// returns index of element in the vector. returns -1 if element is not found;
+template <typename T>
+int Vector<T>::find(T element)
+{
+    for (int i = 0; i < size; i++)
+        if (element == arr[i]) return i;
+    return -1;
+}
+
+// 
+template <typename T>
+int Vector<T>::getSize() 
+{
+    return size;
+}
+
+template <typename T>
+void Vector<T>::push(T element)
+{
+    arr[size] = element;
+    size++;
+
+    redefineCapacity();
+}
+
+template <typename T>
+void Vector<T>::remove(int index)
+{
+    checkBounds(index);
+    size--;
+    for (int i = index; i < size; i++)
+        arr[i] = arr[i+1];
+}
+
+
+template <typename T>
+void Vector<T>::insert(int index, T element)
+{
+    checkBounds(index);
+    
+    for (int i = size; i > index; i--)
+        arr[i] = arr[i-1];
+    arr[index] = element;
+    size++;
+
+    redefineCapacity();
+}
+
+template <typename T>
+void Vector<T>::replace(int index, T element)
+{
+    checkBounds(index);
+    arr[index] = element;
+}
+
+#endif
