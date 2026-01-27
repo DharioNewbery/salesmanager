9f427885fd3f53987ca59e17247a355b863309a5
DharioNewbery <dharionew@gmail.com>
2026-01-27 18:21:03 -0300

    updated example code to use the new SalesManager

--- main.cpp
+++ main.cpp
@@ -5,19 +5,7 @@ using namespace std;
 
 int main () {
     SalesManager sm = SalesManager();
-    Sale a;
-    a.buyer = "Dhario";
-    a.date = "01/01/1970";
-    a.item = "Geladeira Gamer";
-    a.price = 2500;
-    sm.addSale(a);
-
-    a.buyer = "Khey Niwa";
-    a.date = "27/01/2026";
-    a.item = "memoria RAM ddr2 256kb";
-    a.price = 999999;
-    sm.addSale(a);
-
+    sm.loadSales();
     sm.listSales();
     return 0;
 }
