8227a563a095a60690a184f620d56df79d9b22e8
DharioNewbery <dharionew@gmail.com>
2026-01-27 16:37:00 -0300

    Added text examples and functionalities display to main

--- main.cpp
+++ main.cpp
@@ -1,3 +1,23 @@
+#include <iostream>
+#include "Vector.h"
+#include "SalesManager.h"
+using namespace std;
+
 int main () {
+    SalesManager sm = SalesManager();
+    Sale a;
+    a.buyer = "Dhario";
+    a.date = "01/01/1970";
+    a.item = "Geladeira Gamer";
+    a.price = 2500;
+    sm.addSale(a);
+
+    a.buyer = "Khey Niwa";
+    a.date = "27/01/2026";
+    a.item = "memoria RAM ddr2 256kb";
+    a.price = 999999;
+    sm.addSale(a);
+
+    sm.listSales();
     return 0;
 }
