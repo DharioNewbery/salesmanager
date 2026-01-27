1cab29efbf5d8e6b6c56db1f4155fbad878b00e7
DharioNewbery <dharionew@gmail.com>
2026-01-27 18:18:30 -0300

    added parsers for sale struct

--- Sale.h
+++ Sale.h
@@ -1,6 +1,7 @@
 #ifndef SALE_H
 #define SALE_H
 
+#include <sstream>
 #include <iostream>
 
 struct Sale {
@@ -10,7 +11,31 @@ struct Sale {
     std::string buyer;
     std::string date;
 
-    // We can define the display logic right here
+    std::string toCsv() const {
+        return std::to_string(id) + "," + std::to_string(price) + "," + item + "," + buyer + "," + date;    
+    }
+
+    static Sale fromCsv(const std::string& line) {
+        std::stringstream ss(line);
+        std::string temp;
+        Sale s;
+
+        // ORDER MATTERS
+        std::getline(ss, temp, ',');
+        s.id = std::stoi(temp);
+
+        std::getline(ss, temp, ',');
+        s.price = std::stoi(temp);
+
+        std::getline(ss, s.item, ',');
+
+        std::getline(ss, s.buyer, ',');
+
+        std::getline(ss, s.date);
+
+        return s;
+    }
+    
     void display() const {
         std::cout << "ID: " << id << " | Item: " << item
         << " | Price: $" << price << " | Buyer: "
