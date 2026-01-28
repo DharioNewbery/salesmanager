68401ca98772e787ff957163ecb19ab71de384f8
DharioNewbery <dharionew@gmail.com>
2026-01-27 23:17:37 -0300

    Implemented Module SDK for programming modules dinamically

---
+++ ModuleSDK.h
@@ -0,0 +1,32 @@
+#ifndef MODULE_SDK_H
+#define MODULE_SDK_H
+
+#include <string>
+#include <functional>
+#include <map>
+#include <iostream>
+#include "SalesManager.h"
+
+class SalesManager;
+
+typedef std::function<void(SalesManager&)> CommandFunc;
+
+class ModuleRegistry {
+public:
+    
+    static std::map<std::string, CommandFunc>& getCommands() {
+        static std::map<std::string, CommandFunc> commands;
+        return commands;
+    }
+
+    struct Registrator {
+        Registrator(std::string name, CommandFunc func) {
+            std::cout << "new command added: " << name << std::endl;
+            ModuleRegistry::getCommands()[name] = func;
+        }
+    };
+};
+
+#define REGISTER_COMMAND(name, func) \
+    static ModuleRegistry::Registrator global_reg_##func(name, func);
+#endif
