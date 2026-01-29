4944a2462456ecc52c130d2a0900a230ebb1470a
DharioNewbery <dharionew@gmail.com>
2026-01-29 00:32:03 -0300

    extracted input logic to its own file

--- main.cpp
+++ main.cpp
@@ -2,57 +2,35 @@
 #include "Vector.h"
 #include "SalesManager.h"
 #include "ModuleSDK.h"
+#include "inputHandler.h"
 
 using namespace std;
-Vector<string> splitInput(string input) {
-    Vector<string> result = Vector<string>();
-    string currentToken = "";
-    bool insideQuotes = false;
 
-    for (size_t i = 0; i < input.length(); ++i) {
-        char c = input[i];
-
-        if (c == '\"') {
-            // Toggle the state when we hit a quote
-            insideQuotes = !insideQuotes;
-            // Note: We don't add the " character itself to the token
-        } 
-        else if (c == ' ' && !insideQuotes) {
-            // If it's a space and we aren't in quotes, the word is finished
-            if (!currentToken.empty()) {
-                result.push(currentToken);
-                currentToken = "";
-            }
-        } 
-        else {
-            // Otherwise, keep building the current word
-            currentToken += c;
-        }
+void printV(Vector<int> a) {
+    for(int i = 0; i < a.getSize(); i++) {
+        cout << a[i] << " ";
     }
-
-    // Don't forget to add the very last token
-    if (!currentToken.empty()) {
-        result.push(currentToken);
-    }
-
-    return result;
+    cout << endl;
 }
 
 int main () {
     auto& commands = ModuleRegistry::getCommands();
     SalesManager sm = SalesManager();
     sm.loadSales();
-    
+
     bool isRunning = true;
     Vector<string> tokens;
     string input;
-
+    
     while (isRunning) {
         getline(cin, input);
         tokens = splitInput(input);
-        if (tokens[0] == "exit")
+        
+        if (tokens[0] == "exit") {
             isRunning = false;
-        else if (commands.count(tokens[0]))
+            continue;
+        }
+        if (commands.count(tokens[0]))
             commands[tokens[0]](sm, tokens);
         else
             cout << "Unknown command.\n";
