3d26f1ea2e9927d3b9618396b13d4ea657049ecc
DharioNewbery <dharionew@gmail.com>
2026-01-30 12:03:50 -0300

    implemented new help message handling

--- modules/Core.h
+++ modules/Core.h
@@ -1,8 +1,44 @@
 #include "../src/ModuleSDK.h"
 
+namespace help {
+    const auto listSales = "\
+    >> list --help (shows this message)\n\
+    \n\
+    >> list\n\
+    list all sales.\n\
+    \n\
+    >> list <integer start>\n\
+    list all subsequent sales.\n\
+    \n\
+    >> list <integer start> <integer end>\n\
+    list all elements in the interval.\n";
+
+    const auto loadSales = "\
+    >> load --help (shows this message)\n\
+    \n\
+    >> load <filename>\n\
+    load all sales from the file.\n";
+
+    const auto saveSales = "\
+    >> save --help (shows this message)\n\
+    \n\
+    >> save <filename>\n\
+    write all sales to the file.\n";
+    
+    const auto addSale = "\
+    >> add --help (shows this message)\n\
+    \n\
+    >> add\n\
+    Start a routine to add a new sale.\n\
+    \n\
+    >> add <string buyer> <string item> <string date> <int price>\n\
+    Add a new sale.\n";
+}
+
+
+
 void listSales(SalesManager& sm, Vector<std::string> args) {
     int start = 0, end = INT_MAX;
-
     if (args.getSize() > 1) {
         if (!tryParseInt(args[1], start)) {
             std::cerr << "Error: Invalid start index '" << args[1] << "'\n";
@@ -22,6 +58,7 @@ void listSales(SalesManager& sm, Vector<std::string> args) {
 
 void loadSales(SalesManager& sm, Vector<std::string> args) {
     // Basic validation: Command + Filename = 2 total tokens
+    
     if (args.getSize() != 2) {
         std::cerr << "Usage: load <filename>\n";
         return;
@@ -67,14 +104,14 @@ void clear(SalesManager& sm, Vector<std::string> args)
 
 void addSale(SalesManager& sm, Vector<std::string> args) {
     Sale newSale;
-    
+
     // MODE 1: Fast Add (e.g., add "John Doe" "Apple" "01/01/2026" 150)
     if (args.getSize() >= 5) {
         newSale.buyer = args[1];
         newSale.item = args[2];
         newSale.date = args[3];
-        if (tryParseInt(args[4], newSale.price)) {
-            std::cerr << "Error: Invalid price format '" << args[2] << "'\n";
+        if (!tryParseInt(args[4], newSale.price)) {
+            std::cerr << "Error: Invalid price format '" << args[4] << "'\n";
             return;
         } 
     } 
@@ -95,8 +132,8 @@ void addSale(SalesManager& sm, Vector<std::string> args) {
         while (!isPriceValid) {
             std::cout << "Valor: ";
             std::getline(std::cin, input);
-            if (tryParseInt(input, newSale.price)) {
-                std::cerr << "Error: Invalid price format '" << args[2] << "'\n";
+            if (!tryParseInt(input, newSale.price)) {
+                std::cerr << "Error: Invalid price format '" << input << "'\n";
             } else isPriceValid = true;
         }
     }
@@ -106,6 +143,11 @@ void addSale(SalesManager& sm, Vector<std::string> args) {
     else
         std::cout << "Error: Manager could not store the sale.\n";
 }
+
+REGISTER_HELP_MESSAGE("list", help::listSales, listSales)
+REGISTER_HELP_MESSAGE("load", help::loadSales, loadSales)
+REGISTER_HELP_MESSAGE("save", help::saveSales, saveSales)
+REGISTER_HELP_MESSAGE("add", help::addSale, addSale)
 
 REGISTER_COMMAND("list", listSales)
 REGISTER_COMMAND("load", loadSales)
