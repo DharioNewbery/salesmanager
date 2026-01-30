bea6b69d8f7f71b711b66f9d57cb8104758fa1eb
DharioNewbery <dharionew@gmail.com>
2026-01-30 12:01:08 -0300

    Implemented support to help commands directly on the SDK

--- src/ModuleSDK.h
+++ src/ModuleSDK.h
@@ -15,19 +15,34 @@ typedef std::function<void(SalesManager&, Vector<std::string>)> CommandFunc;
 class ModuleRegistry {
 public:
     
-    static std::map<std::string, CommandFunc>& getCommands() {
-        static std::map<std::string, CommandFunc> commands;
+    static std::unordered_map<std::string, std::string>& getHelpMessages() {
+        static std::unordered_map<std::string, std::string> helpMessages;
+        return helpMessages;
+    }
+
+    struct HelpRegistrator {
+        HelpRegistrator(const char* name, const char* message) {
+            std::cout << "help message registered for command '" << name << "'\n";
+            ModuleRegistry::getHelpMessages()[name] = message;
+        }
+    };
+
+    static std::unordered_map<std::string, CommandFunc>& getCommands() {
+        static std::unordered_map<std::string, CommandFunc> commands;
         return commands;
     }
 
-    struct Registrator {
-        Registrator(std::string name, CommandFunc func) {
+    struct CommandRegistrator {
+        CommandRegistrator(std::string name, CommandFunc func) {
             std::cout << "new command added: " << name << std::endl;
             ModuleRegistry::getCommands()[name] = func;
         }
     };
 };
 
+#define REGISTER_HELP_MESSAGE(name, message, func) \
+    static ModuleRegistry::HelpRegistrator global_reg_help_##func(name, message);
+
 #define REGISTER_COMMAND(name, func) \
-    static ModuleRegistry::Registrator global_reg_##func(name, func);
+    static ModuleRegistry::CommandRegistrator global_reg_##func(name, func);
 #endif
