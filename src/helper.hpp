bbc93555b2325db2a1a26f0075ac55e0740be02e
DharioNewbery <dharionew@gmail.com>
2026-02-19 14:03:49 -0300

    added helper function to convert string into price accounting to commas and dots

--- src/helper.hpp
+++ src/helper.hpp
@@ -3,48 +3,66 @@
 
 #include <string>
 #include <stdexcept>
+namespace price {
+    bool getPrice(const std::string& date, int& value) {
+        std::string s_value;
+        
+        size_t index = date.find('.');
+        if (index == -1) index = date.find(',');
+        if (index == -1) index = 0;
 
-bool getDMY(const std::string& date, int& day, int& month, int& year) {
-    std::string s_day, s_month, s_year;
-    s_day = date.substr(0, 2);
-    s_month = date.substr(2, 2);
-    s_year = date.substr(4, 2);
+        s_value = date.substr(index);
 
-    try
-    {
-        day = std::stoi(s_day);
-        month = std::stoi(s_month);
-        year = std::stoi(s_year);
+        try {
+            value = std::stoi(s_value);
+        }
+        catch (...) { return false; }
+
+        return true;
+    } 
+}
+namespace date {
+    bool getDMY(const std::string& date, int& day, int& month, int& year) {
+        std::string s_day, s_month, s_year;
+        s_day = date.substr(0, 2);
+        s_month = date.substr(2, 2);
+        s_year = date.substr(4, 2);
+
+        try
+        {
+            day = std::stoi(s_day);
+            month = std::stoi(s_month);
+            year = std::stoi(s_year);
+        }
+        catch(...) { return false; }
+
+        return true;
     }
-    catch(...) { return false; }
 
-    return true;
+    bool isDateValid(const std::string& date)
+    {
+        int day, month, year;
+        if (!getDMY(date, day, month, year)) return false;
+        
+        if ((0 > day || day > 31)
+        || (0 > month || month > 12)
+        || (1000 > year || year > 9999)
+        ) return false;
+        
+        return true;
+    }
+
+    unsigned long dateToMillis(const std::string& date)
+    {
+        if (!isDateValid(date)) throw std::invalid_argument("The given string is not a valid date");
+
+        int day, month, week;
+    }
+
+    std::string millisToDate(unsigned long dateMillis)
+    {
+        return std::string();
+    }
 }
-
-bool isDateValid(const std::string& date)
-{
-    int day, month, year;
-    if (!getDMY(date, day, month, year)) return false;
-    
-    if ((0 > day || day > 31)
-    || (0 > month || month > 12)
-    || (1000 > year || year > 9999)
-    ) return false;
-    
-    return true;
-}
-
-unsigned long dateToMillis(const std::string& date)
-{
-    if (!isDateValid(date)) throw std::invalid_argument("The given string is not a valid date");
-
-    int day, month, week;
-}
-
-std::string millisToDate(unsigned long dateMillis)
-{
-    return std::string();
-}
-
 
 #endif
